6ea332eaa0aab1565a021538a1d129dcc96f95c3
anthonypmm <purcellaanthony@gmail.com>
2025-08-29 12:10:48 -0700

    Installed Vite/React + Connected Frontend with Backend Server

--- server/express-server.js
+++ server/express-server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 3000;
 import * as db from "../db/index.js";
 import idExists from "./middleware/idExists.js";
 import baseURLExtract from "../utils/baseURLExtract.js";
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
